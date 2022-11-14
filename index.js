3a936d3f2a7dcffcbe6bce0db839e140db46d711
sealite92 <87363693+sealite92@users.noreply.github.com>
2022-11-14 09:12:45 +0100

    I added an else statement

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ let a = 5;
 let b = 10;
 let c = a + b;
 
-if (c > 5) console.log('i am a decagon aspirant');
+if (c > 5) console.log('i am a decagon aspirant')
+else {console.log('github is very good for this programne}
